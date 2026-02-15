2a25fca9de0f378b7e931d8e637b55a209304513
Mohamed <moesam1456@gmail.com>
2026-02-15 15:49:06 +0200

    tesst

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -1,95 +1,95 @@
-"use client";
+'use client'
 
-import Image from "next/image";
-import Bounded from "../Bounded";
-import Button from "../Button";
-import TextSplitter from "../TextSplitter";
+import Image from 'next/image'
+import Bounded from '../Bounded'
+import Button from '../Button'
+import TextSplitter from '../TextSplitter'
 
-import { View } from "@react-three/drei";
-import { Bubbles } from "../Bubbles";
-import { useStore } from "@/hooks/useStore";
-import { useMediaQuery } from "@/hooks/useMediaQuery";
-import HeroScene from "./HeroScene";
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { View } from '@react-three/drei'
+import { Bubbles } from '../Bubbles'
+import { useStore } from '@/hooks/useStore'
+import { useMediaQuery } from '@/hooks/useMediaQuery'
+import HeroScene from './HeroScene'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
-gsap.registerPlugin(useGSAP, ScrollTrigger);
+gsap.registerPlugin(useGSAP, ScrollTrigger)
 
 const Hero = () => {
-  const ready = useStore((state) => state.ready);
-  const isDesktop = useMediaQuery("(min-width:768px)", true);
+  const ready = useStore((state) => state.ready)
+  const isDesktop = useMediaQuery('(min-width:768px)', true)
 
   useGSAP(
     () => {
-      if (!ready && isDesktop) return;
+      if (!ready && isDesktop) return
 
-      const introTl = gsap.timeline();
+      const introTl = gsap.timeline()
 
       introTl
-        .set(".hero", { opacity: 1 })
-        .from(".hero-header-word", {
+        .set('.hero', { opacity: 1 })
+        .from('.hero-header-word', {
           scale: 3,
           opacity: 0,
-          ease: "power4.in",
+          ease: 'power4.in',
           delay: 0.3,
           stagger: 1,
         })
         .from(
-          ".hero-subheading",
+          '.hero-subheading',
           {
             opacity: 0,
             y: 30,
           },
-          "+=.8"
+          '+=.8'
         )
-        .from(".hero-body", {
+        .from('.hero-body', {
           opacity: 0,
           y: 10,
         })
-        .from(".hero-button", {
+        .from('.hero-button', {
           opacity: 0,
           y: 10,
           duration: 0.6,
-        });
+        })
 
       const scrollTl = gsap.timeline({
         scrollTrigger: {
-          trigger: ".hero",
-          start: "top top",
-          end: "bottom bottom",
+          trigger: '.hero',
+          start: 'top top',
+          end: 'bottom bottom',
           scrub: 1.5,
         },
-      });
+      })
 
       scrollTl
         .fromTo(
-          "body",
+          'body',
           {
-            backgroundColor: "#FDE047",
+            backgroundColor: '#FDE047',
           },
           {
-            backgroundColor: "#D9F99D",
-            overwrite: "auto",
+            backgroundColor: '#D9F99D',
+            overwrite: 'auto',
           },
           1
         )
-        .from(".text-side-heading .split-char", {
+        .from('.text-side-heading .split-char', {
           scale: 1.3,
           y: 40,
           rotate: -25,
           opacity: 0,
           stagger: 0.1,
-          ease: "back.out(3)",
+          ease: 'back.out(3)',
           duration: 0.5,
         })
-        .from(".text-side-body", {
+        .from('.text-side-body', {
           y: 20,
           opacity: 0,
-        });
+        })
     },
     { dependencies: [ready, isDesktop] }
-  );
+  )
 
   return (
     <Bounded className="hero opacity-0">
@@ -115,11 +115,7 @@ const Hero = () => {
             <div className="hero-body text-2xl font-mono text-sky-950">
               3-5g sugar. 9g fiber.5 delicious flavors.
             </div>
-            <Button
-              buttonLink="/"
-              buttonText="SHOP NOW"
-              className="hero-button mt-12"
-            />
+            <Button buttonLink="/" buttonText="SHOP NOW" className="hero-button mt-12" />
           </div>
         </div>
 
@@ -136,15 +132,15 @@ const Hero = () => {
               <TextSplitter text="Try all five flavors" />
             </h2>
             <div className="text-side-body mt-4 max-w-xl text-balance text-xl font-normal text-sky-950">
-              Our soda is made with real fruit juice and a touch of cane sugar.
-              We never use artificial sweeteners or high fructose corn syrup.
-              Try all five flavors and find your favorite!
+              Our soda is made with real fruit juice and a touch of cane sugar. We never use
+              artificial sweeteners or high fructose corn syrup. Try all five flavors and find your
+              favorite!
             </div>
           </div>
         </div>
       </div>
     </Bounded>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
